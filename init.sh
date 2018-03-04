8c97879b8dd832d0c201c3745bcd83ac2bfb5686
Vadym Panchenko <panchenko.vadym@gmail.com>
2018-03-04 10:48:22 +0200

    upd docker files & added env example file

--- init.sh
+++ init.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
+# Create default .ENV config
+# --------------------------
+#cp .env_example .env
+sudo sed -i '3i 10.0.3.1    mongocli' /etc/hosts
+
 # Start
 # -----
-docker-compose build apache2 workspace mongo php-fpm
+docker-compose build apache2 mongo php-fpm workspace
 docker-compose up -d apache2 mongo
 docker-compose exec mongo sh /mongo.sh user password
 
+# Execute workspace with install dependencies & run CLI
+# -----------------------------------------------------
+docker-compose exec workspace composer install && php cli.php
+
 # Execute workspace
 # -----------------
-docker-compose exec workspace php cli.php
+#docker-compose exec workspace php cli.php
 
 # Execute specified containers
 # ----------------------------
