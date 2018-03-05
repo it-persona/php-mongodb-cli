a58635fca4411ae78e256a77129176516ae034f7
Vadym Panchenko <panchenko.vadym@gmail.com>
2018-03-05 08:29:49 +0200

    update host ip script and phpunit to version 6.5

--- ip.sh
+++ ip.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # find all these tokens variables
 base_url_token="{{ base_url }}"
@@ -6,7 +6,12 @@ host_ip_token="{{ host_ip }}"
 
 # replace with public url & host ip
 base_url="mongocli"
-host_ip=$(ip route get 1 | awk '{print $NF;exit}');
+host_ip="";
+#host_ip=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | awk '{print $NF;exit}');
+
+if [[ -z ${host_ip} ]]; then
+    host_ip=$(ip route get 1 | awk '{print $NF;exit}');
+fi
 
 # output
 # ------
